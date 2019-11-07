aaa0070d1c69839576e9297f06219eb126a6ca8a
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2019-11-07 19:07:42 -0300

    format shell script using shfmt

--- vendor/pybind11/tools/check-style.sh
+++ vendor/pybind11/tools/check-style.sh
@@ -18,35 +18,34 @@
 check_style_errors=0
 IFS=$'\n'
 
-found="$( GREP_COLORS='mt=41' GREP_COLOR='41' grep $'\t' include tests/*.{cpp,py,h} docs/*.rst -rn --color=always )"
+found="$(GREP_COLORS='mt=41' GREP_COLOR='41' grep $'\t' include tests/*.{cpp,py,h} docs/*.rst -rn --color=always)"
 if [ -n "$found" ]; then
-    # The mt=41 sets a red background for matched tabs:
-    echo -e '\033[31;01mError: found tab characters in the following files:\033[0m'
-    check_style_errors=1
-    echo "$found" | sed -e 's/^/    /'
+	# The mt=41 sets a red background for matched tabs:
+	echo -e '\033[31;01mError: found tab characters in the following files:\033[0m'
+	check_style_errors=1
+	echo "$found" | sed -e 's/^/    /'
 fi
 
-
-found="$( grep -IUlr $'\r' include tests/*.{cpp,py,h} docs/*.rst --color=always )"
+found="$(grep -IUlr $'\r' include tests/*.{cpp,py,h} docs/*.rst --color=always)"
 if [ -n "$found" ]; then
-    echo -e '\033[31;01mError: found CRLF characters in the following files:\033[0m'
-    check_style_errors=1
-    echo "$found" | sed -e 's/^/    /'
+	echo -e '\033[31;01mError: found CRLF characters in the following files:\033[0m'
+	check_style_errors=1
+	echo "$found" | sed -e 's/^/    /'
 fi
 
-found="$(GREP_COLORS='mt=41' GREP_COLOR='41' grep '[[:blank:]]\+$' include tests/*.{cpp,py,h} docs/*.rst -rn --color=always )"
+found="$(GREP_COLORS='mt=41' GREP_COLOR='41' grep '[[:blank:]]\+$' include tests/*.{cpp,py,h} docs/*.rst -rn --color=always)"
 if [ -n "$found" ]; then
-    # The mt=41 sets a red background for matched trailing spaces
-    echo -e '\033[31;01mError: found trailing spaces in the following files:\033[0m'
-    check_style_errors=1
-    echo "$found" | sed -e 's/^/    /'
+	# The mt=41 sets a red background for matched trailing spaces
+	echo -e '\033[31;01mError: found trailing spaces in the following files:\033[0m'
+	check_style_errors=1
+	echo "$found" | sed -e 's/^/    /'
 fi
 
 found="$(grep '\<\(if\|for\|while\|catch\)(\|){' include tests/*.{cpp,h} -rn --color=always)"
 if [ -n "$found" ]; then
-    echo -e '\033[31;01mError: found the following coding style problems:\033[0m'
-    check_style_errors=1
-    echo "$found" | sed -e 's/^/    /'
+	echo -e '\033[31;01mError: found the following coding style problems:\033[0m'
+	check_style_errors=1
+	echo "$found" | sed -e 's/^/    /'
 fi
 
 found="$(awk '
@@ -62,9 +61,9 @@ last && /^\s*{/ {
 { last = /(if|for|while|catch|switch)\s*\(.*\)\s*$/ ? $0 : "" }
 ' $(find include -type f) tests/*.{cpp,h} docs/*.rst)"
 if [ -n "$found" ]; then
-    check_style_errors=1
-    echo -e '\033[31;01mError: braces should occur on the same line as the if/while/.. statement. Found issues in the following files:\033[0m'
-    echo "$found"
+	check_style_errors=1
+	echo -e '\033[31;01mError: braces should occur on the same line as the if/while/.. statement. Found issues in the following files:\033[0m'
+	echo "$found"
 fi
 
 exit $check_style_errors
